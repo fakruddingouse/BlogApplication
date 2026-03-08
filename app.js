e581419f4d1d0b89009600e8990aae3549e116f9
Baba Fakruddin <abdulahmadsab123@gmail.com>
2026-03-08 11:58:12 +0530

    Added MongoDB Atlas connection and deployment fixes

--- app.js
+++ app.js
@@ -14,10 +14,15 @@ const { checkForAuthenticationCookie } = require('./middlewares/authentication')
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+if (!process.env.MONGO_URI) {
+    console.log("MONGO_URI not found");
+    process.exit(1);
+}
+
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then((e) => console.log("MongoDB Connected"))
-    .catch((error) => console.log(error.message));
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((error) => console.log(error));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve('./views'));
@@ -28,16 +33,21 @@ app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 
 app.get('/', async (req, res) => {
-    const allBlogs = await Blog.find({});
-    res.render('home', {
-        user: req.user,
-        blogs: allBlogs
-    });
-})
+    try {
+        const allBlogs = await Blog.find({});
+        res.render('home', {
+            user: req.user,
+            blogs: allBlogs
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Server Error");
+    }
+});
 
 app.use('/user', userRoute);
 app.use('/blog', blogRoute);
 
 app.listen(PORT, () => 
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on port ${PORT}`)
 );
